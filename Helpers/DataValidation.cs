1b35bd45b283ce64d7458d7bb93ae50b55baaca3
Sophaneth Sy <sysophaneth@gmail.com>
2025-12-31 12:45:09 +0700

    Helper Functions for Validating data and Filtering features

---
+++ Helpers/DataValidation.cs
@@ -0,0 +1,13 @@
+﻿using Student_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Student_Management.Helpers
+{
+    public static class DataValidation
+    {
+
+    }
+}
---
+++ Helpers/ReturnStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Student_Management.Helpers
+{
+    public enum ReturnStatus
+    {
+        Success,
+        Pending,
+        Failed,
+        Rejected,
+    }
+}
